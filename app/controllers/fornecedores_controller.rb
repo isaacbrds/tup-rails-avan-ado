621cef66469e461fc7e96b3cd259b0e14ac0e0d3
isaac <isaac.brigido@gmail.com>
2024-05-04 15:30:45 -0300

    feat: criando forms na mão

--- app/controllers/fornecedores_controller.rb
+++ app/controllers/fornecedores_controller.rb
@@ -1,5 +1,52 @@
 class FornecedoresController < ApplicationController
+  before_action :carregar_fornecedor, only: %i[editar alterar excluir]
   def index
     @fornecedores = Fornecedor.all
+  end
+
+  def novo
+    @fornecedor = Fornecedor.new
+  end
+
+  def editar
+    
+  end
+
+  def alterar
+    debugger
+    if @fornecedor.update(parametros_de_fornecedor)
+      flash[:notice] = "Cadastro atualizado com sucesso!"
+      redirect_to fornecedores_path
+    else
+      flash[:alert] = "Fornecedor não realizado!"
+      redirect_to fornecedores_editar_path(@fornecedor)
+    end
+  end
+
+  def cadastrar
+    fornecedor = Fornecedor.new(parametros_de_fornecedor)
+    if fornecedor.save
+      flash[:notice] = "Cadastro realizado com sucesso!"
+      redirect_to fornecedores_path
+    else
+      flash[:alert] = "Cadastro não realizado!"
+      redirect_to fornecedores_novo_path
+    end
+  end
+
+  def excluir
+    @fornecedor.destroy
+    flash[:notice] = "Exclusão realizado com sucesso!"
+    redirect_to fornecedores_path
+  end
+
+  private
+
+  def carregar_fornecedor
+    @fornecedor = Fornecedor.find(params[:id])
+  end
+
+  def parametros_de_fornecedor
+    params.require(:fornecedor).permit(:nome, :cnpj, :endereco)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'fornecedores', to: 'fornecedores#index'
+  get 'fornecedores/novo', to: 'fornecedores#novo'
+  post 'fornecedores', to: 'fornecedores#cadastrar', as: 'fonecedores_cadastrar'
+  get "fornecedores/:id/editar", to: 'fornecedores#editar', as: 'fornecedores_editar'
+  post "fornecedores/:id/alterar", to: 'fornecedores#alterar', as: 'fornecedores_alterar'
+  delete "fornecedores/:id", to: 'fornecedores#excluir', as: 'fornecedores_excluir'
   resources :clientes
   get 'stimulus', to: 'spa#index'
   root "home#index"
